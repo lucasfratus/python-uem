41b9b18a1f81ee5adb9365dbbe0ace5535471033
lucasfratus <134955580+lucasfratus@users.noreply.github.com>
2023-08-01 22:13:02 -0300

    Update polegada_centimetro.py

--- projetos primeiro semestre/polegada_centimetro.py
+++ projetos primeiro semestre/polegada_centimetro.py
@@ -9,6 +9,17 @@ def main():
     print(polegada,'polegada(s) convertido para centímetros é',centimetro)
 
 def polegada_centimetro(polegada: float) -> float:
+    '''
+    Converte a medida em polegadas em centímetros.
+    
+    Exemplos
+    >>> polegada_centimetro(1)
+    2.54
+    >>> polegada_centimetro(2)
+    5.08
+    >>> polegada_centimetro(3)
+    7.62
+    '''
     return polegada * 2.54
 
 main()
