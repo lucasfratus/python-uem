235c8616fe338139d0a9b384d1dce23fd7ab1ec2
lucasfratus <134955580+lucasfratus@users.noreply.github.com>
2023-08-21 19:50:26 -0300

    .

--- projetos primeiro semestre/bandeira_energia.py
+++ projetos primeiro semestre/bandeira_energia.py
@@ -1,11 +1,11 @@
 # Análise
-# Determinar o valor final da tarifa de energia, considerando o consumo quilowatt-hora, o valor do kW/h
+# Determinar o valor final da tarifa de energia, considerando o consumo quilowatt-hora, o valor do kW/h \
 # e a bandeira tarifaria.
 #
 # Tipos de Dados
 # O consumo em quilowatt-hora será dado em um número flutuante positivo.
 # O valor do quiilowatt-hora será um número flutuante positivo.
-# A bandeira tarifária terá seu tipo próprio.
+# A bandeira tarifária terá seu tipo próprio tipo.
 
 from enum import Enum, auto
 class bandeira(Enum):
@@ -16,12 +16,30 @@ class bandeira(Enum):
     VERMELHA2:auto()
 
 
-def valor_energia(tarifa_original,kwh : float,bandeira_tarifaria: bandeira) -> str:
+def valor_energia(tarifa_original, kwh, valor_bandeira: float,bandeira_tarifaria: bandeira) -> str:
     '''
-    Calcula o valor final da tarifa de energia, somando o valor da *tarifa_original* com o produto 
-    entre o consumo em kW/h e o valor da *bandeira_tarifaria*
-    
+    Calcula o valor final da tarifa de energia, somando o valor da *tarifa_original* com o produto \ 
+    entre o consumo em kW/h e o valor da *bandeira_tarifaria*. A bandeira tarifária possui 4 tipos: VERDE \
+    (tarifa não sofre acrescimo), AMARELA(acréscimo de R$ 0,01874 por quilowatt-hora), VERMELHA - patamar 1(acréscimo \
+    de R$ 0,03971 por quilowatt-hora) e VERMELHA - patamar 2(acréscimo de R$ 0,09492 por quilowatt-hora).
     Exemplos:
-    >>> valor_energia(10,10,VERDE)
-    10
-    >>> valor_energia(20.10,AMARELA)
+    >>> valor_energia(100.00,10,bandeira.VERDE)
+    100.00
+    >>> round(valor_energia(200.00,100.00,bandeira.AMARELA),2)
+    201.87
+    >>> round(valor_energia(150.00,110.00,bandeira.VERMELHA1),2)
+    154.37
+    >>> round(valor_energia(170.00,130.00,bandeira.VERMELHA2),2)
+    182.34
+    '''
+    if bandeira_tarifaria == bandeira.VERDE:
+        valor_bandeira = 0
+    if bandeira_tarifaria == bandeira.AMARELA:
+        valor_bandeira = 0.01874
+    if bandeira_tarifaria == bandeira.VERMELHA1:
+        valor_bandeira = 0.03971
+    else:
+        valor_bandeira = 0.09492
+    
+    return tarifa_original + (kwh * valor_bandeira)
+    
